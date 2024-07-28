f907f9d8f723dd8b269e3271a79d6fb7585a040b
Katie Loesch <catherine.loesch@protonmail.com>
2024-07-28 18:50:50 +0100

    use context to conditionally render different links in navbar

--- src/components/shared/Nav/NavLinks/NavLinks.js
+++ src/components/shared/Nav/NavLinks/NavLinks.js
@@ -1,23 +1,35 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
 
+import { AuthContext } from '../../context/authContext';
 import './NavLinks.scss';
 
 const NavLinks = () => {
+  const auth = useContext(AuthContext);
+
   return (
     <ul className='nav-links'>
       <li>
         <NavLink to='/'>All Users</NavLink>
       </li>
-      <li>
-        <NavLink to='/u0/places'>My Places</NavLink>
-      </li>
-      <li>
-        <NavLink to='/places/new'>Add new place</NavLink>
-      </li>
-      <li>
-        <NavLink to='/auth'>Log In / Register</NavLink>
-      </li>
+
+      {auth.isLoggedIn && (
+        <li>
+          <NavLink to='/u0/places'>My Places</NavLink>
+        </li>
+      )}
+
+      {auth.isLoggedIn && (
+        <li>
+          <NavLink to='/places/new'>Add new place</NavLink>
+        </li>
+      )}
+
+      {!auth.isLoggedIn && (
+        <li>
+          <NavLink to='/auth'>Log In / Register</NavLink>
+        </li>
+      )}
     </ul>
   );
 };
